0baadf975c741678cbf4a105b36f2391e149c490
sedihglow <jamesmross4@gmail.com>
2020-11-25 20:46:45 -0800

    moisure sensor test

--- esp/tests/moisture/moisture.ino
+++ esp/tests/moisture/moisture.ino
@@ -1,15 +1,35 @@
+#include "Adafruit_seesaw.h"
+#include <Wire.h>
 
+#define SCL 2
+#define SDA 14
+#define I2C_ADDR 0x36
+#define TR_PIN 0 //pin used in touchRead()
+
+Adafruit_seesaw ss;
 
 void setup()
 {
+    Serial.begin(115200);
+    Serial.println("seesaw soil sensor example.");
 
+    Wire.begin(SCL, SDA);
 
-
+    if (!ss.begin(I2C_ADDR)) {
+        Serial.println("ERROR seesaw not found");
+        while(true);
+    } else {
+        Serial.print("seesaw started. version: ");
+        Serial.println(ss.getVersion(), HEX);
+    }
 }
 
 void loop()
 {
+    float tempC = ss.getTemp();
+    uint16_t capRead = ss.touchRead(0);
 
-
-
+    Serial.print("Temperature: "); Serial.print(tempC); Serial.println("*C");
+    Serial.print("Capacitive: "); Serial.println(capRead);
+    delay(100);
 }
